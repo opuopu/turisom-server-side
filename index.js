0bfd32e8bf9d47edf2242548a4b6c87fe02f9162
opu34 <nurmdopu428@gmail.com>
2021-10-29 02:09:29 +0600

    done

--- index.js
+++ index.js
@@ -10,16 +10,48 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.envDB_USER}:${process.env.DB_PASS}@cluster0.dgoei.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dgoei.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 client.connect(err => {
-    const database = client.db("ASSIGNMENT-11")
-    const collections=database.collection("services");
+    const database = client.db("DELIVERY")
+    const Servicecollections=database.collection("services");
+    const Bookingcollection = database.collection('mybooking')
     
 
-// -------------------
+// -------------------post-------
+
+app.post('/addservice', async(req,res)=>{
+   
+    const result = await  Servicecollections.insertOne(req.body)
+    res.send(result)
+})
+
+// ----------------------get--------
+app.get('/getservice', async(req,res)=>{
+    const result = await Servicecollections.find({}).toArray()
+    res.send(result)
+
+})
+// --------------------post for booking-----------
+app.post('/mybooking',async(req,res)=>{
+    console.log(req.body);
+    const result = await  Bookingcollection.insertOne(req.body)
+    res.send(result)
+})
+app.get('/findbooking/:email', async(req,res)=>{
+   
+    const result =  await Bookingcollection.find({email:req.params.email}).toArray()
+    res.send(result)
+})
+
+// -------------------single---------
+app.get('/single/:id', async(req,res)=>{
+    console.log(req.params.id);
+    const result = await Servicecollections.findOne({_id:ObjectId(req.params.id)})
+    res.send(result)
+})
 console.log("database");
 })
 
